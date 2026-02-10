87023be5f7774db9bf3ddb8e6a3a87c0b10f0ec1
AntoniaChroni <achroni@noderome260.cm.cluster>
2026-02-09 18:25:37 -0600

    Update volcano plot function

--- analyses/de-go-analysis/util/function-create-volcano-plot.R
+++ analyses/de-go-analysis/util/function-create-volcano-plot.R
@@ -10,10 +10,10 @@
 #'
 #' @examples
 #' 
-crete_volcano_plot <- function(all.markers,
+create_volcano_plot <- function(all.markers,
                                n_value,
                                plots_dir) {
-
+  
   # ---- Optional: per-cluster volcano plots ----
   group_col <- "cluster"        # change to your grouping column if different
   stopifnot(group_col %in% names(all.markers))
@@ -58,29 +58,28 @@ crete_volcano_plot <- function(all.markers,
     select_labels_grp <- select_labels_grp[!is.na(select_labels_grp) & nzchar(select_labels_grp)]
     
     cat("Volcano Plot:", cl, "\n")
-    p_grp <- print(EnhancedVolcano::EnhancedVolcano(
-      df,
-      lab            = df$label,            # <- gene names, not numbers
-      x              = "avg_log2FC",
-      y              = "p_val_adj",
-      selectLab      = select_labels_grp,   # <- same label space as 'lab'
-      drawConnectors = TRUE,
-      parseLabels    = FALSE,     # If labels still parse as math, disable parsing:
-      col            = c("black","black","black","red3"),     # Make significant points red only; others black
-      pCutoff        = 5e-2,
-      FCcutoff       = 0.25,
-      title          = paste("Volcano Plot:", cl),
-      subtitle       = NULL,
-      ylab           = bquote(~-Log[10]~ 'P adj'),
-      # Optional label/point tweaks
-      labSize        = 3.3,
-      pointSize      = 1.2,
-      max.overlaps   = 25
-    ))
+    p_grp <- print(EnhancedVolcano::EnhancedVolcano(df,
+                                                    lab = df$gene,     # rownames(df), if unique or df$gene
+                                                    x = "avg_log2FC", #log2FoldChange  
+                                                    y = "p_val", # EnhancedVolcano expects raw p-values here, because the function applies -log10 internally
+                                                    selectLab      = select_labels_grp,   # <- same label space as 'lab'
+                                                    drawConnectors = TRUE,
+                                                    parseLabels    = FALSE,     # If labels still parse as math, disable parsing:
+                                                    #col            = c("black","black","black","red3"),     # Make significant points red only; others black
+                                                    #pCutoff = 5e-2, # adjusted or raw p-value threshold;  often use p_val_adj threshold
+                                                    #FCcutoff       = 0.25,
+                                                    title          = paste("Volcano Plot:", cl),
+                                                    subtitle       = NULL,
+                                                    ylab           = bquote(~-Log[10]~ 'P adj'),
+                                                    # Optional label/point tweaks
+                                                    labSize        = 3.3,
+                                                    pointSize      = 1.2,
+                                                    max.overlaps   = 25
+                                                    )) 
     
     out_png_cl <- file.path(plots_dir, glue::glue("Volcano-plot-{cl}.png"))
     ggplot2::ggsave(filename = out_png_cl, plot = p_grp, width = 8, height = 6, dpi = 300)
   }
-
+  
 }
 ############################################################################################################
